1c866b3d2d70aeddb3e1a85ea9617549ae9d760a
Ozmostar <ozmostar77@gmail.com>
2024-12-30 21:12:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+import random
+
+def generate_password(numOfChars):
+    password = ""
+    for i in range(numOfChars):
+        password += random.choice(karakterler)
+    return password
+    
+num_of_letters = int(input("kaç karakterli bir şifre istiyorsun?"))
+
+password = generate_password(num_of_letters)
+
+print(password)
+
